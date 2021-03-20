76b3c164705a7e48f8f8e9bd6c7022ca155c85a0
Gabriel Simmons <simmons.gabe@gmail.com>
2021-03-19 18:36:42 -0700

    add some requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     py_modules=['papercast'],
     install_requires=[
         'click',
+        'spacy',
+        'en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.0.0/en_core_web_sm-2.0.0.tar.gz',
+        'beautifulsoup4',
+        'mutagen'
     ],
     entry_points='''
         [console_scripts]
